d47155338af4a5521310a3e61a745bd2aa7f34e9
J.Cole <ccjohn015@gmail.com>
2022-02-06 16:55:28 -0600

    finished generate page

--- utils/generateMark.js
+++ utils/generateMark.js
@@ -30,3 +30,39 @@ function renderLicenseSection(license) {
     `
 }
 
+function generateMarkdown(data) {
+    // console.log(data)
+    return `
+    # ${data.title}
+    ${renderLicenseBadge(data.license)}
+    ## Description
+    ${data.description}
+    ## Table of Contents
+    * [Installation](#installation)
+    * [Usage](#usage)
+    ${renderLicenseLink(data.license)}
+    * [Contributing](#contributing)
+    * [Tests](#tests)
+    * [Questions](#questions)
+    
+    ## Installation
+    To install necessary dependencies, run the following command: 
+    \`\`\`
+    ${data.installation}
+    \`\`\`
+    ## Usage
+    ${data.usage}
+    
+    ${renderLicenseSection(data.license)}
+    ## Contributing
+    ${data.contribution}
+    ## Tests
+    ${data.test}
+    ## Questions
+    Contact me for any questions. You can send me an email at ${data.email}.
+    
+    Link to my GitHub: github.com/${data.username}
+  `;
+}
+  
+  module.exports = generateMarkdown;
